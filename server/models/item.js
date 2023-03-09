1ae1dd877a6298fe901f467b2a37c091c4a0cc2b
Davies-98 <davies98@protonmail.com>
2023-03-09 11:19:20 +0000

    fix

--- server/models/item.js
+++ server/models/item.js
@@ -1,34 +1,21 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+module.exports = (sequelize, DataTypes) => {
+  const Item = sequelize.define('Item', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
 
-const Item = sequelize.define('Item', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.FLOAT,
-    allowNull: false
-  },
-  category: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  quantity: {
-    type: DataTypes.FLOAT,
-    allowNull: false
-  },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-});
-
-module.exports = Item;
+  return Item;
+};
